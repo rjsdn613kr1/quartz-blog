6d561524c3742c4ac02e40e84c5989a540f9e706
rjsdn613 <rjsdn613@naver.com>
2026-04-02 21:23:51 +0900

    vault backup: 2026-04-02 21:23:51

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "ggoungwoo",
+    pageTitle: "suko_yasa",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
